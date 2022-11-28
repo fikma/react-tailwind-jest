7b54f14fd0a2a2f96347e62e6355f8aaabe81624
fikma <ade.fm97@gmail.com>
2022-11-28 19:28:59 +0700

    Starter react dengan tailwind+jest test

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import './app.css'
+
+const App = () => {
+    return (
+        <h1 className="text-primary text-4xl font-bold">
+            Hello world
+        </h1>
+    )
+}
+export default App
